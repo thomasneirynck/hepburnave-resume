5f3a01ca400d6fe409f8c7292377ef44b09e5ec9
trgn <thomas@neirynck.us>
2015-09-19 00:04:54 -0400

    add release script

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function (grunt) {
+    require("load-grunt-tasks")(grunt);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        compress: {
+            main: {
+                options: {
+                    archive: "release/hepburnave.zip"
+                },
+                files: [
+                    {cwd: ".", src: "index.html", dest: "hepburnave", filter: "isFile"},
+                    {cwd: ".", src: "main.css", dest: "hepburnave", filter: "isFile"},
+                    {cwd: ".", src: "images/**", dest: "hepburnave", filter: "isFile"}
+                ]
+            }
+        }
+    });
+    grunt.registerTask('default', ['compress']);
+};
